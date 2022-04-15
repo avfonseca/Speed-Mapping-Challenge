f506ad388e63b0e59787355fc0da1350f72f1560
Adriano Fonseca <afonseca@ccom.unh.edu>
2022-04-15 14:53:47 -0400

    QGIS raster

--- test/test.py
+++ test/test.py
@@ -1,94 +1,152 @@
+from tkinter import N
 from osgeo import gdal
 from osgeo import osr
 import numpy as np
-import pygmt
+import os
+import sys
 
-# import 
+def open_raster(file_name, band_number):
 
-ds = gdal.Open('NONNA10_5300N13170W.bag')
-driver = gdal.GetDriverByName('BAG')
+    try:
+        raster = gdal.Open(file_name)
+    except RuntimeError as e:
+        print("ERROR: Cannot open raster.")
+        print(e)
+        return None
 
-#Band with data
-band = ds.GetRasterBand(1)
-bag = -1*band.ReadAsArray()
-
-#Mask of valid soundings
-no_data_val = -1000000.0
-valid_mask = np.where(bag == no_data_val,0,1) 
-
-
-
-
-###Color Codes###
-A1 = [143.0/255.0, 191.0/255.0, 147.0/255.0]
-A2 = [129.0/255.0, 195.0/255.0, 226.0/255.0]
-A3 = [129.0/255.0, 195.0/255.0, 226.0/255.0]
-A4 = [167.0/255.0, 224.0/255.0, 233.0/255.0]
-A5 = [182.0/255.0, 235.0/255.0, 219.0/255.0]
-A6 = [216.0/255.0, 244.0/255.0, 225.0/255.0]
-A7 = [216.0/255.0, 244.0/255.0, 225.0/255.0]
-A8 = [216.0/255.0, 244.0/255.0, 225.0/255.0]
-A9 = [161.0/255.0,74.0/255.0,55.0/255.0]
+    try:
+        raster_band = raster.GetRasterBand(band_number)
+    except RuntimeError as e:
+        print("ERROR: Cannot access raster band.")
+        print(e)
+        return None
+    return raster, raster_band
 
 
-bag_color = np.zeros((*bag.shape,3))
+def main(band_number, input_file):
+    src, band = open_raster(input_file,band_number)
+    
+    nodata = band.GetNoDataValue()
+    (upper_left_x, x_size, x_rotation, upper_left_y, y_rotation, y_size) = src.GetGeoTransform()
 
-print(bag_color.shape)
+    numpy_array = src.ReadAsArray()
 
-bag_color[np.where(bag<=0)] = np.array(A1) # CHM < 0 - Class 1
-bag_color[np.where((bag>0) & (bag<=2))] = np.array(A2) # 0m < CHM < 2m - Class 2
-bag_color[np.where((bag>2) & (bag<=5))] = np.array(A3) # 2m < CHM < 5m - Class 3
-bag_color[np.where((bag>5) & (bag<=10))] = np.array(A4) # 5m < CHM < 10m - Class 4
-bag_color[np.where((bag>10) & (bag<=20))] = np.array(A5) # 10m < CHM < 20m - Class 5
-bag_color[np.where((bag>20) & (bag<=30))] = np.array(A6) # 20m < CHM < 30m - Class 6
-bag_color[np.where((bag>30) & (bag<=50))] = np.array(A7) # 30m < CHM < 50m - Class 7
-bag_color[np.where((bag>50) & (bag<=100))] = np.array(A8) # 50m < CHM - Class 8
-bag_color[np.where(bag==no_data_val)] = np.array(A9) # no data val - Class 9
+    #width,height = numpy_array.shape
+    width= band.XSize
+    height= band.YSize
+
+    # add a feature
+    fet = QgsFeature()
+
+    for row in range(height):
+        for col in range(width):
+            if  (nodata == numpy_array[row,col]):
+        #               #Add new point:
+                x = col * x_size + upper_left_x + (x_size / 2) #add half the cell size
+                y = row * y_size + upper_left_y + (y_size / 2) #to centre the point
+                fet.setGeometry(QgsGeometry.fromPoint(QgsPoint(x,y)))
+                fet.initAttributes(3)
+                fet.setAttribute(0, float(numpy_array[row,col]))
+                fet.setAttribute(1, row)
+                fet.setAttribute(2, col)
+                pr.addFeatures( [ fet] )
 
 
-
-
-# getting projection from source raster
-srs = osr.SpatialReference()
-srs.ImportFromWkt(ds.GetProjectionRef())
-# create layer with projection
-out_layer = out_data.CreateLayer(raster_path.split('.')[0], srs)        
-new_field = ogr.FieldDefn('field_name', ogr.OFTReal)
-out_layer.CreateField(new_field)        
-gdal.FPolygonize(bag_color, bag_color, out_layer, 0, [], callback=None)        
-source_raster = None
+# Commit changes
+    vl.commitChanges()
 
 
 
 
 
-#fig = pygmt.Figure()
-#fig.grdcontour(grid=bag)
-#fig.savefig("test.png")
 
 
 
 
 
-#plt.figure(); 
-#plt.imsave("test.png",bag_color)
-#plt.title('SERC CHM Classification')
-#ax=plt.gca(); ax.ticklabel_format(useOffset=False, style='plain') 
 
 
-#bag[valid_mask==0] = 0 
 
-#Normalize Image for saving
-#range = (np.max(bag) - np.min(bag))
-#bag = (bag - np.min(bag))/range
 
-#bag = np.expand_dims(bag, axis=2)
-#valid_mask = np.expand_dims(valid_mask, axis=2)
-#rgb_bag = np.repeat(bag, 3, axis=2)
 
-#output = Image.new(mode="RGBA", size=(bag.shape[0],bag.shape[1]))
-#output = np.concatenate((rgb_bag,valid_mask),axis=2)
-#output = np.uint8(output*255)
 
-#img = Image.fromarray(output, 'RGBA')            
-#img.save('my2.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # make standalone
+    if len( sys.argv ) < 3:
+        print("""
+        ERROR: Provide two arguments:
+        1) the band number (int) and 2) input raster directory (str)
+        """)
+
+    main(int(sys.argv[1]), str(sys.argv[2]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ###Color Codes###
+# A1 = [143.0/255.0, 191.0/255.0, 147.0/255.0]
+# A2 = [129.0/255.0, 195.0/255.0, 226.0/255.0]
+# A3 = [129.0/255.0, 195.0/255.0, 226.0/255.0]
+# A4 = [167.0/255.0, 224.0/255.0, 233.0/255.0]
+# A5 = [182.0/255.0, 235.0/255.0, 219.0/255.0]
+# A6 = [216.0/255.0, 244.0/255.0, 225.0/255.0]
+# A7 = [216.0/255.0, 244.0/255.0, 225.0/255.0]
+# A8 = [216.0/255.0, 244.0/255.0, 225.0/255.0]
+# A9 = [161.0/255.0,74.0/255.0,55.0/255.0]
+
+
+# bag_color = np.zeros((*bag.shape,3))
+
+# bag_color[np.where(bag<=0)] = np.array(A1) # CHM < 0 - Class 1
+# bag_color[np.where((bag>0) & (bag<=2))] = np.array(A2) # 0m < CHM < 2m - Class 2
+# bag_color[np.where((bag>2) & (bag<=5))] = np.array(A3) # 2m < CHM < 5m - Class 3
+# bag_color[np.where((bag>5) & (bag<=10))] = np.array(A4) # 5m < CHM < 10m - Class 4
+# bag_color[np.where((bag>10) & (bag<=20))] = np.array(A5) # 10m < CHM < 20m - Class 5
+# bag_color[np.where((bag>20) & (bag<=30))] = np.array(A6) # 20m < CHM < 30m - Class 6
+# bag_color[np.where((bag>30) & (bag<=50))] = np.array(A7) # 30m < CHM < 50m - Class 7
+# bag_color[np.where((bag>50) & (bag<=100))] = np.array(A8) # 50m < CHM - Class 8
+# bag_color[np.where(bag==no_data_val)] = np.array(A9) # no data val - Class 9
